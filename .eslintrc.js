677179e99798888e52b275229681bffe40014614
Pedro Goiânia <pedro.paulods@hotmail.com>
2022-04-06 11:41:21 -0300

    feat(config): added new configs to project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,40 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es6: true,
+    jest: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'module-resolver'],
+  rules: {
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.js'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'no-param-reassign': 'off',
+    'no-console': 'off',
+    'module-resolver/use-alias': ['error', {}],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
+  },
 };
---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'type-enum': [2, 'always', ['feat', 'fix', 'test', 'style']],
+  },
+};
